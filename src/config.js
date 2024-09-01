d970717e4d0ce5966c7f72f9aab26e4827e3db14
Ssempijja Charles Charz <ssempijacharz@gmail.com>
2024-09-01 23:18:38 +0300

    Add project structure and initial files

---
+++ src/config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    colors: [
+        { name: 'dark', color: '#000000' },
+        { name: 'light', color: '#FFFFFF' },
+        { name: 'blue', color: '#0000FF' },
+        { name: 'red', color: '#FF0000' },
+        { name: 'green', color: '#00FF00' },
+        { name: 'yellow', color: '#FFFF00' },
+        { name: 'purple', color: '#800080' },
+        { name: 'orange', color: '#FFA500' },
+    ],
+    iconSize: 24, // Size of the icon canvas in pixels
+    outputFormats: ['svg', 'png'], // Add 'png' if you want to generate PNG versions as well
+};
